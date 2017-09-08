da052a1fe1fa23cdfab7d8336d0f52487ac2a3a1
Jose Ospina <pepo.ospina@gmail.com>
2017-09-08 10:09:53 +0200

    Working nicely

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w3-container">
     <h1>Remittances App</h1>
-    <h3>Total balance {{ contractBalance }} eth</h3>
+    <h3>Total App Balance {{ contractBalance }} eth</h3>
     <div class="w3-row from-row">
       <label class=""><b>From:</b></label>
       <select class="w3-input" v-model="from">
@@ -12,7 +12,7 @@
     </div>
 
     <div class="w3-row">
-      <h5>current balance: {{ fromBalance.toFixed(2) }} eth</h5>
+      <h5>address balance: {{ fromBalance.toFixed(2) }} eth</h5>
     </div>
 
     <div class="w3-row w3-margin-top">
@@ -21,7 +21,7 @@
         Send
       </button>
       <button class="w3-col s6 w3-bottombar w3-button" :class="{'w3-border-blue': !sendRemittance}"
-        @click="sendRemittance = false">
+        @click="switchToReceive()">
         Receive
       </button>
     </div>
@@ -65,7 +65,7 @@
       </div>
 
       <div class="w3-row-padding w3-margin-top">
-        <label><b>Secrets Hash String</b></label>
+        <label><b>Secrets Hash</b></label>
         <input v-model="secretsHash" type="text" class="w3-input" disabled>
       </div>
 
@@ -96,12 +96,35 @@
         </div>
       </div>
     </div>
+
+    <hr>
+    <h2>Remittances History</h2>
+    <table class="w3-table w3-bordered w3-centered">
+
+      <tr>
+        <th>State</th>
+        <th>Amount (eth)</th>
+        <th class="hashCol">id</th>
+        <th class="hashCol">Ex. Hash</th>
+      </tr>
+
+      <tr v-for="remittance in remittancesHistory">
+        <td class="w3-tag w3-padding"
+          :class="{'w3-green': isPaid(remittance.args.secretsHash), 'w3-red': !isPaid(remittance.args.secretsHash)}">
+          <b>{{ isPaid(remittance.args.secretsHash) ? 'PAID' : 'PENDING' }}</b>
+        </td>
+        <td>{{ Number(web3.fromWei(remittance.args.amount, 'ether')) }}</td>
+        <td class="hashCol">{{ remittance.args.secretsHash }}</td>
+        <td class="hashCol">{{ remittance.args.exchangerAddressHash }}</td>
+      </tr>
+
+    </table>
+
   </div>
 </template>
 
 <script>
 import { Remittance, web3 } from '@/web3-loader.js'
-import { keccak256 } from 'js-sha3'
 
 export default {
   data () {
@@ -119,11 +142,26 @@ export default {
       contractBalance: '0',
       paymentProcessed: false,
       paymentSuccesfull: true,
-      fromBalance: 0
+      fromBalance: 0,
+      remittancesHistory: [],
+      paymentsHistory: []
+    }
+  },
+
+  computed: {
+    web3 () {
+      return web3
     }
   },
 
   methods: {
+    switchToReceive () {
+      this.sendRemittance = false
+      this.secret1 = ''
+      this.secret2 = ''
+      this.secretsHash = ''
+      this.exchangerAddressHash = ''
+    },
     updateContractBalance () {
       web3.eth.getBalance(this.instance.address, (err, balance) => {
         if (err) {
@@ -149,10 +187,10 @@ export default {
       console.log(secret1)
       console.log(secret2)
 
-      var secretsHash = '0x' + keccak256(secret1 + secret2)
+      var secretsHash = web3.sha3(secret1 + secret2)
       console.log(secretsHash)
 
-      var exchangerHash = '0x' + keccak256(this.exchangerAddress)
+      var exchangerHash = web3.sha3(this.exchangerAddress, {encoding: 'hex'})
       console.log(exchangerHash)
 
       this.instance.sendRemittance(secretsHash, exchangerHash, { from: this.from, value: web3.toWei(this.amount, 'ether') })
@@ -187,9 +225,14 @@ export default {
           this.paymentSuccesfull = true
           this.secret1 = ''
           this.secret2 = ''
+          this.secretsHash = ''
           this.exchangerAddressHash = ''
           this.updateContractBalance()
           this.updateFromBalance()
+          setTimeout(() => {
+            this.paymentProcessed = false
+            this.paymentSuccesfull = false
+          }, 5000)
         } else {
           console.log('error sending remittance')
           this.paymentSuccesfull = false
@@ -198,6 +241,30 @@ export default {
         this.paymentProcessed = true
         this.paymentSuccesfull = false
         console.log(err)
+      })
+    },
+    isPaid (secretsHash) {
+      for (var ix in this.paymentsHistory) {
+        if (this.paymentsHistory[ix].args.secretsHash === secretsHash) {
+          return true
+        }
+      }
+
+      return false
+    },
+    configureEvents () {
+      this.instance.LogRemittanceSent({}, { fromBlock: 0 }).watch((err, event) => {
+        if (err) {
+          console.log(err)
+        }
+        this.remittancesHistory.push(event)
+      })
+
+      this.instance.LogRemittancePaid({}, { fromBlock: 0 }).watch((err, event) => {
+        if (err) {
+          console.log(err)
+        }
+        this.paymentsHistory.push(event)
       })
     }
   },
@@ -212,6 +279,7 @@ export default {
     Remittance.deployed().then((instance) => {
       this.instance = instance
       this.updateContractBalance()
+      this.configureEvents()
     })
 
     web3.eth.getAccountsPromise().then((accounts) => {
@@ -232,6 +300,11 @@ export default {
 
 .send-row button {
   min-width: 300px;
+}
+
+.hashCol {
+  max-width: 200px;
+  overflow-x: auto;
 }
 
 </style>
